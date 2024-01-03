0a99425fa8bc7f44f5e4c6d7ce38d91ca9eb576e
Unova <unova.x98@gmail.com>
2024-01-02 22:24:29 -0600

    Update back-end code to use ES modules and add API routes

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,19 @@
-//archivo index.js
-var express = require('express');
-var app = express();
 
-const PUERTO = 8001;
+import express from 'express';
+import cors from 'cors';
+import apiRoutes from './routes/index.js';
 
-app.listen(PUERTO, function(){
-	console.log('Servidor http correindo en el puerto ' + PUERTO);
-});
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', function(req, res){
-    res.send('Hola mundo');
+// Configuraciones y middleware
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.use('/api', apiRoutes);
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor en ejecución en http://localhost:${PORT}`);
 });
